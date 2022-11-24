5893492925710d1a41e670e6ea7129c4c8515b84
Billy Jones <24443637+Hellcat404@users.noreply.github.com>
2022-11-24 11:29:58 +0000

    Add project files.

---
+++ BillyJonesUK/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace BillyJonesUK.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
